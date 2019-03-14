cd8e03b205e66dc2e8687ea575dcb002444961de
FeruzOripov <akaferuz99@gmail.com>
2019-03-14 20:43:23 +0500

    Adding comments for our code.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Blog struct for service.
 type Blog struct {
 	ID           int        `json:"id"`
 	Title        string     `json:"title"`
@@ -17,42 +18,54 @@ type Blog struct {
 	CreatedYear  int        `json:"createdYear"`
 }
 
+// Initializing id. Each time creating new post it is incremented
 var id int
 
+// Initiliazing some posts.
 var posts = []Blog{
 	Blog{id, "Hello, World!", "This is test", time.Now().Day(), time.Now().Month(), time.Now().Year()},
 }
 
 func main() {
 	r := gin.Default()
+	// Defining a group named /api
 	api := r.Group("/api")
 	{
+		// Test url that responses with Hello, World!
 		api.GET("/", sayHello)
+		// Lists all posts
+		api.GET("/posts", getPosts)
+		// Get a post with given id
+		api.GET("/posts/:id", getPost)
+		// Post a new post
+		api.POST("/posts", postPost)
+		// Update a post with given id
+		api.POST("/posts/:id", updatePost)
+		// Delete a post with a given id
+		api.DELETE("posts/:id", deletePost)
 	}
-	api.GET("/posts", getPosts)
-	api.GET("/posts/:id", getPost)
-	api.POST("/posts", postPost)
-	api.POST("/posts/:id", updatePost)
-	api.DELETE("posts/:id", deletePost)
 	r.Run()
 }
 
+// responses with Hello, World
 func sayHello(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "Hello, World!",
 	})
 }
 
+// List all posts
 func getPosts(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
 	c.JSON(http.StatusOK, posts)
 }
 
+// Get a post with given id
 func getPost(c *gin.Context) {
 	if postID, err := strconv.Atoi(c.Param("id")); err == nil {
 		for _, post := range posts {
 			if post.ID == postID {
-				//c.Header("Content-Type", "application/json")
+				c.Header("Content-Type", "application/json")
 				c.JSON(http.StatusOK, post)
 				return
 			}
@@ -61,10 +74,11 @@ func getPost(c *gin.Context) {
 	}
 }
 
+// Create a new post
 func postPost(c *gin.Context) {
 	var post Blog
 	c.Bind(&post)
-	id++
+	id++ // Increment an id for new post
 	post.ID = id
 	post.CreatedDay = time.Now().Day()
 	post.CreatedMonth = time.Now().Month()
@@ -73,6 +87,7 @@ func postPost(c *gin.Context) {
 	c.JSON(201, post)
 }
 
+// Update a post
 func updatePost(c *gin.Context) {
 	var post Blog
 	c.Bind(&post)
@@ -90,11 +105,12 @@ func updatePost(c *gin.Context) {
 	}
 }
 
+// Delete a post
 func deletePost(c *gin.Context) {
 	if postID, err := strconv.Atoi(c.Param("id")); err == nil {
 		for i, post := range posts {
 			if post.ID == postID {
-				posts = RemoveIndex(posts, i)
+				posts = removeIndex(posts, i)
 				c.Header("Content-Type", "application/json")
 				c.JSON(http.StatusOK, posts)
 				return
@@ -104,6 +120,8 @@ func deletePost(c *gin.Context) {
 	}
 }
 
-func RemoveIndex(s []Blog, index int) []Blog {
+// Function for removing element from array.
+// Because our posts are in array.
+func removeIndex(s []Blog, index int) []Blog {
 	return append(s[:index], s[index+1:]...)
 }
